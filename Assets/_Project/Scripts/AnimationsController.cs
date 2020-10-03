d62bc773e1908576bcee29dcfa3bf45f9b192a48
Pedro Marangon <pedrommarangon@gmail.com>
2020-10-02 22:37:03 -0300

    Added animation script

---
+++ Assets/_Project/Scripts/AnimationsController.cs
@@ -0,0 +1,47 @@
+﻿// Maded by Pedro M Marangon
+using UnityEngine;
+
+namespace IndianaLoopes.Animations
+{
+
+    [System.Serializable]
+    public enum AnimationString
+	{
+        Idle,
+        Walk
+	}
+
+    [System.Serializable]
+    public struct Animations
+	{
+        public string name;
+        public AnimationString type;
+	}
+
+    public class AnimationsController : MonoBehaviour
+    {
+
+        [SerializeField] private Animator anim = null;
+
+		[SerializeField] private Animations[] animations = new Animations[0];
+
+		// Start is called before the first frame update
+		void Awake()
+        {
+            if (!anim) anim = GetComponentInChildren<Animator>();
+        }
+
+        public void SetAnimation(AnimationString animation)
+		{
+			foreach (Animations a in animations)
+			{
+                if (!a.type.Equals(animation) ||
+                    anim.GetCurrentAnimatorClipInfo(0)[0].clip.name.Equals(a.name)) continue;
+
+                anim?.Play(a.name);
+                break;
+			}
+		}
+    }
+
+}
